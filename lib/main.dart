23e1b72a81ffed7b22311df9ef6aed34bfd3d79f
OkonoWil <okonowilfried@gmail.com>
2022-08-17 07:22:47 +0100

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,95 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController = TabController(length: 5, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: const Text(
+              'facebook',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 30, 127, 206),
+                  fontSize: 35,
+                  fontWeight: FontWeight.w700),
+            ),
+            actions: <Widget>[
+              IconButton(
+                  icon: const Icon(Icons.add_outlined),
+                  color: Colors.grey,
+                  onPressed: () {}),
+              IconButton(
+                  icon: const Icon(Icons.search_rounded),
+                  color: Colors.grey,
+                  onPressed: () {}),
+              IconButton(
+                  icon: const Icon(Icons.message_sharp),
+                  color: Colors.grey,
+                  onPressed: () {})
+            ],
+            bottom: TabBar(
+              controller: _tabController,
+              labelColor: Colors.blue,
+              tabs: const <Widget>[
+                Tab(
+                  icon: Icon(Icons.home),
+                ),
+                Tab(icon: Icon(Icons.live_tv_rounded)),
+                Tab(icon: Icon(Icons.store_outlined)),
+                Tab(icon: Icon(Icons.notifications_none_rounded)),
+                Tab(icon: Icon(Icons.menu))
+              ],
+            )),
+        body: TabBarView(controller: _tabController, children: const <Widget>[
+          Center(
+              child: Text(
+            'Bienvenue sur mon clône de Facebook',
+            style: TextStyle(color: Colors.blue, fontSize: 20),
+          )),
+          Center(
+              child: Text(
+            'Watch',
+            style: TextStyle(color: Colors.blue, fontSize: 20),
+          )),
+          Center(
+              child: Text(
+            'Marketplace',
+            style: TextStyle(color: Colors.blue, fontSize: 20),
+          )),
+          Center(
+              child: Text(
+            'Notification',
+            style: TextStyle(color: Colors.blue, fontSize: 20),
+          )),
+          Center(
+              child: Text(
+            'Menu',
+            style: TextStyle(color: Colors.blue, fontSize: 20),
+          )),
+        ]));
   }
 }
